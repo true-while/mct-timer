459419024e3f2792659a7c2a4c30aa26549dbee1
Alex ivanov <aivanofff@gmail.com>
2024-08-04 19:09:06 -0400

    editor

---
+++ mtt-timer/wwwroot/js/editor.js
@@ -0,0 +1,18 @@
+﻿const toolbarOptions = [
+    [{ 'size': ['normal', 'large', 'huge'] }],
+    [{ 'header': 1 }, { 'header': 2 }],
+    ['bold', 'italic'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'color': [] }, { 'background': [] }],
+];
+
+
+const options = {
+    debug: 'info',
+    modules: {
+        toolbar: toolbarOptions
+    },
+    placeholder: '',
+    theme: 'snow'
+};
+const quill = new Quill('#editor', options);
