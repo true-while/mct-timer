6c7a86a197a6cb2793bd1e3569696fd946c86f43
Alex ivanov <aivanofff@gmail.com>
2024-12-30 14:22:02 -0500

    remove old websettings

--- mct-timer/Controllers/ImgSrvController.cs
+++ mct-timer/Controllers/ImgSrvController.cs
@@ -85,6 +85,65 @@ namespace mct_timer.Controllers
         }
 
 
+        [HttpPost]
+        public async Task<IActionResult> ImgGenerate()
+        {
+            BinaryData events = await BinaryData.FromStreamAsync(HttpContext.Request.Body);
+            _tmClient.TrackTrace($"Received events: {events}");
+
+            return new OkObjectResult("OK");
+
+           
+            /* 
+             * EventGridEvent[] eventGridEvents = EventGridEvent.ParseMany(events);
+
+            foreach (EventGridEvent eventGridEvent in eventGridEvents)
+            {
+                // Handle system events
+                if (eventGridEvent.TryGetSystemEventData(out object eventData))
+                {
+                    ImgSrvData data;
+                    try
+                    {
+                        data = System.Text.Json.JsonSerializer.Deserialize<ImgSrvData>(Encoding.UTF8.GetString(eventGridEvent.Data));
+                    }
+                    catch (Exception ex)
+                    {
+                        _tmClient.TrackException(ex);
+                        return new BadRequestObjectResult("Unrecognized event");
+                    }
+
+                    if (data.validationCode != null)
+                        return new OkObjectResult("{'validationResponse':'" + data.validationCode + "'}");
+
+                    var requesterName = (new Uri(_config.Value.StorageAccountName).Host).Split('.')[0];
+
+                    if (!eventGridEvent.Topic.Contains(requesterName) || !eventGridEvent.Topic.Contains(_config.Value.SubscriptionID))
+                    {
+                        _tmClient.TrackException(new FormatException("event not recognized " + requesterName));
+                        return new OkObjectResult("event not recognized "); //validation does not looks good but do not retry trigger
+                    }
+                    _tmClient.TrackTrace(data.url);
+
+                    if (data != null && data.url.Contains(BlobRepo.LaregeImgfolder))
+                    {
+                        string fileName = Path.GetFileName(data.url);
+
+                        var mResult = await _blRepo.TransformMediumFileAsync(fileName);
+                        var sResult = await _blRepo.TransformSmallFileAsync(fileName);
+
+
+                        _tmClient.TrackTrace($"Processed SubscriptionValidation event data with result: {sResult & mResult}, topic: {eventGridEvent.Topic}");
+
+                    }
+
+                }
+            }
+            return new OkObjectResult("OK"); // im ok, do not retry trigger
+            */
+
+        }
+
 
     }
 }
